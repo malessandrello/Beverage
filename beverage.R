5d3e1a19b7b50099030ce53b6fab172c024ff1c4
Mauricio Alessandrello <mauricio.alessandrello@gmail.com>
2025-09-29 19:27:04 -0300

    Updates

---
+++ beverage.R
@@ -0,0 +1,32 @@
+library(tidyverse)
+library(readr)
+
+data <- read_csv("Study Data.csv")
+
+data %>% 
+  filter(`Question Name` == "daily_alc" & `Participant ID` == 13)
+  
+
+data %>% 
+  filter(`Question Name` == "week_alc_use" & `Participant ID` == 13)
+
+data %>% 
+  filter(str_detect(`Question Name`, "who") & `Participant ID` == 13)
+
+
+daily_alc <- data %>% 
+  drop_na() %>% 
+  filter(`Question Name` == "daily_alc") %>% 
+  group_by(`Participant ID`, Battery) %>%
+  summarise(Value = mean(Value))
+
+daily_alc %>% 
+  ggplot(aes(Battery, Value))+
+  geom_boxplot()
+
+daily_alc_w <- daily_alc %>% 
+  pivot_wider(names_from = Battery, values_from = Value)
+
+t.test(daily_alc_w$`Beverage Daily`, daily_alc_w$`Pre Beverage Daily`)
+
+mean(is.na(data))
